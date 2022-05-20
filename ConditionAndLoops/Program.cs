77c9dc2718272864316e604b02f830a412a0fca1
maliyev2 <maliyev2@stu.vistula.edu.pl>
2022-05-20 19:30:05 +0400

    First commit

---
+++ ConditionAndLoops/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace ConditionAndLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1)Istifadəçidən alınacaq iki ədəd arasinda ədədlərin toplamını ekranda göstərəcək
+            //C# kodunu yazıniz
+            //Console.WriteLine("Birinci  ededi daxil edin");
+            //int StartPoint = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Ikinci ededi daxil edin");
+            //int EndPoint = Convert.ToInt32(Console.ReadLine());
+            //int sum = 0;
+            //for (int i = StartPoint; i <EndPoint; i++)
+            //{
+
+            //    sum = sum + i;
+
+
+            //}
+            //Console.WriteLine("Reqemlerin cemi:" + sum);
+
+
+            //2) Istifadəçidən alınacaq iki ədəd   arasındadakı ədədlərin  ortlamasını tapan və 
+            //  bu ortalamanı ekrana yazdıran
+            //  C# kodunu yazınız 
+
+
+            //Console.WriteLine("Birinci ededi daxil edin");
+            //float Start = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Ikinci ededi daxil edin");
+            //float End = Convert.ToInt32(Console.ReadLine());
+            //float cem = 0;
+            //float avg = 0;
+            //float count = 0;
+            //for (float i = Start; i < End; i++)
+            //{
+            //    cem = cem + i;
+            //    count++;
+            //    avg = cem / count;
+            //}
+            //Console.WriteLine(avg);
+
+
+            //3)Bir kurs qeydiyyat proqramı yazdığınızı düşünün. 
+            // İlk şərt kursa qeydiyyat etdirmək istəyən şəxslərin yaşını soruşulur 
+            // əgər şəxsin yaşı 18 + üzəri isə bunun üçün bir sonrakı şərt olan texniki ingiliscə balını soruşulur.
+            // Bu bal 50 + isə bu şəxsi kurs üçün qeydiyyata alınır 
+            // əks halda bir sonrakı tələbə varmı deyə kontrol edən C# kodunu yazınız 
+
+            //bool IsContinue = true;
+            //do
+            //{
+            //    Console.Write("Yashinizi qeyd edin: ");
+            //    int age = int.Parse(Console.ReadLine());
+            //    if (age>18)
+            //    {
+            //        Console.Write("Ingilis dili balini qeyd edin: ");
+            //        int point = int.Parse(Console.ReadLine());
+
+            //        if (point>50)
+            //        {
+            //            Console.WriteLine("Tebrikler siz artiq kursun telebesisiz");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Ingilis baliniz yeterli deyil");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Yashiniz uygun deyil");
+            //    }
+
+            //    Console.Write("Novbeti telebe varmi?: ");
+            //    string NovbetiTelebe = Console.ReadLine();
+            //    if (NovbetiTelebe == "He")
+            //    {
+            //        IsContinue = true;
+            //    }
+            //    else if (NovbetiTelebe == "Yox")
+            //    {
+            //        Console.WriteLine("Novbeti telebe yoxdur");
+            //        IsContinue = false;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Zehmet olmasa duzgun qeyd edin");
+            //        IsContinue = false;
+            //    }
+
+
+
+
+            //} while (IsContinue);
+
+
+
+
+
+
+
+
+
+            //4) Istifadəçidən alınacaq iki ədəd arasındakı tək ədədlərin cəmini tapan 
+            //və bu cəmi ekranda göstərən C# kodunu yazınız
+
+            //    int a = 0;
+            //    Console.Write("Birinci reqemi daxil edin: ");
+            //    a = int.Parse(Console.ReadLine());
+            //    int b = 0;
+            //    Console.Write("Ikinci reqemi daxil edin: ");
+            //    b = int.Parse(Console.ReadLine());
+            //    int sum = 0;
+
+            //    for (int i = a; i < b ; i += 2)
+            //    {
+            //        sum += i;
+            //    }
+            //    Console.Write($"Sum of odd numbers from {a} to {b} is : {sum}");
+            //    Console.ReadLine();
+
+            //}
+
+
+
+
+
+
+        }
+    }
+}
+    
+
